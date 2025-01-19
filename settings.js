3548f4302bca59e1b7beac952d7e862822ed0c1f
AXELLNUGROHOzy12 <fadilahalhabsyi78@gmail.com>
2025-01-19 21:24:53 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,12 +7,12 @@ const chalk = require('chalk');
  */
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 //settings owner di ./setown.js
-global.packname = 'SychBotz'
-global.owner = ["62882008702155"]
-global.botnum = ["6287862997267"]
-global.author = 'ydaa'
-global.owname = 'mzyda'
-global.botname = 'SYCHY BOTz'
+global.packname = 'RAMSSMD'
+global.owner = ["6287873650820"]
+global.botnum = ["6285961125834"]
+global.author = 'FADIL'
+global.owname = 'FADIL'
+global.botname = 'RANSS BOTz'
 global.themeemoji = '🪀'
 global.f = '> '
 global.n = '`'
